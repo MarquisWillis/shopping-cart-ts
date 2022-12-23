ad4eea8f004aecaf2ed5295393f78d27a18cd7c7
Marquis Willis <marquiswillis99@gmail.com>
2022-12-23 18:49:00 -0500

    added functionality to shopping cart itself, where the items its data are being shown depending on selection including total number of units, total price, etc

---
+++ src/components/CartItem.tsx
@@ -0,0 +1,41 @@
+import { Button, Stack } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
+import storeItems from "../data/items.json";
+import { formatCurrency } from "../utils/formatCurrency";
+
+type CartItemProps = {
+  id: number;
+  quantity: number;
+};
+
+export function CartItem({ id, quantity }: CartItemProps) {
+  const { removeFromCart } = useShoppingCart();
+  const item = storeItems.find((i) => i.id === id);
+
+  if (item == null) return null;
+
+  return (
+    <Stack direction="horizontal" gap={2}>
+      <img
+        src={item.imgUrl}
+        style={{ width: "125px", height: "75px", objectFit: "cover" }}
+        alt="cart-item"
+      />
+      <div className="me-auto">
+        <div>
+          {item.name}{" "}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              {quantity}x
+            </span>
+          )}
+        </div>
+        <div className="text-muted" style={{ fontSize: ".75rem" }}>
+          {formatCurrency(item.price)}
+        </div>
+        <div> {formatCurrency(item.price * quantity)} </div>
+        <Button variant="danger" size="sm" onClick={() => removeFromCart(id)}>&times;</Button>
+      </div>
+    </Stack>
+  );
+}
