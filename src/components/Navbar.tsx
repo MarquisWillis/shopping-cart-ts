77680bfafc42260a6759c1e9fa6b098751675d05
Marquis Willis <marquiswillis99@gmail.com>
2022-12-23 19:42:56 -0500

    added localStorage functionality to the cart, so on refresh the cart items will still be present

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,7 +28,7 @@ export function Navbar() {
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            xmlns:xlink="http://www.w3.org/1999/xlink"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
             version="1.1"
             viewBox="0 0 512 512"
             enable-background="new 0 0 512 512"
